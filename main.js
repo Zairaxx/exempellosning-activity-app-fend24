ce2cd93052fc1fdd790b15bee91001f486cf8063
Zairaxx <brandontsegai@gmail.com>
2025-01-31 12:51:23 +0100

    added comments

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 //Local storage
 
+//När sidan laddas gör vi följande:
+
+//Kollar om värde i sessionStorage finns
 console.log(sessionStorage.getItem("active"));
 
 
+//Kollar om värde i localStorage finns
 
 if(localStorage.getItem("myObj")){
-
+    //Om värde finns - gör om strängen vi sparat till ett objekt
     let obj = JSON.parse(localStorage.getItem("myObj"))
 
-    console.log(obj);
-
+    //Sen kan vi skriva ut värden från objektet
     let greeting = document.createElement("h2");
     greeting.textContent = ` Welcome ${obj.name} - ${obj.age}` 
     document.body.append(greeting)
@@ -18,18 +21,24 @@ if(localStorage.getItem("myObj")){
 }
 
 document.querySelector("button").addEventListener("click", () => {
-
+    
+    //Spara värde i session storage vid klick
     sessionStorage.setItem("active", true)
 
 })
 
-// JSON Stringify + parse - Hantera och lagra objekt med data
+// JSON.stringify & JSON.parse
 
+// Vi kan endast spara strängar i local/session storage.
+// För att spara data (objekt, arrays) måste vi göra om dem till strängar först
+// Det kan vi göra med metoden JSON.stringify()
 let obj = {
     name: "Brandon",
     age: 30
 }
 
+    // Vi lagrar inte objektet direkt, utan vi gör om det till en sträng först
+
 localStorage.setItem("myObj", JSON.stringify(obj));
-//Session storage
+
 
