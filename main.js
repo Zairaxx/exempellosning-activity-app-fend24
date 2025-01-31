fda059be8b045806060e4772b0969fe275f5a69e
Zairaxx <brandontsegai@gmail.com>
2025-01-31 14:16:07 +0100

    added localStorage

--- main.js
+++ main.js
@@ -22,6 +22,21 @@ const newActivity = async () => {
     let response = await fetch(`https://bored.api.lewagon.com/api/activity?${params}`);
     let activity = await response.json();
 
+    //Spara aktivitet i localStorage
+    localStorage.setItem("activity", JSON.stringify(activity));
+
+    //Spara filterOptions i localStorage
+
+    let options = {
+        type,
+        participants,
+        free
+    }
+
+    console.log(options);
+
+    localStorage.setItem("options", JSON.stringify(options));
+
     document.querySelector(".activity-container").innerHTML = `
         <h2>${activity.activity}</h2>
         <p>Participants: ${activity.participants}</p>
@@ -30,3 +45,37 @@ const newActivity = async () => {
     `
 }
 
+const onLoad = () => {
+
+    //Kolla om det finns options från senaste besöket
+
+    if(localStorage.getItem("options")){
+
+        let options = JSON.parse(localStorage.getItem("options"));
+        //Participants
+        let previousSelected = document.querySelector(`[name='participants'][value="${options.participants}"]`)
+        previousSelected.checked = true
+
+        //Type
+        document.querySelector("select#activityType").value = options.type
+
+        //Free
+        document.querySelector("#free").checked = options.free;
+    }
+
+    //Kolla om det finns aktivitet i localStorage
+    if(localStorage.getItem("activity")){
+        let activity = JSON.parse(localStorage.getItem("activity"));
+     
+        //Skriva ut aktivitet i DOM:en
+        document.querySelector(".activity-container").innerHTML = `
+        <h2>${activity.activity}</h2>
+        <p>Participants: ${activity.participants}</p>
+        <p>Type: ${activity.type}</p>
+        <p>Price: ${activity.price}</p>
+    `
+    }
+    
+}
+
+onLoad();
